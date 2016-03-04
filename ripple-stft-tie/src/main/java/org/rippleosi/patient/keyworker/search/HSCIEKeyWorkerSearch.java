150cfa2afe4cdfb9741a9850ee1800a141f42abd
danielledanderson <anderson.danielle92@gmail.com>
2016-03-04 15:32:16 +0000

    IDCR-591 Get Key Worker Route implementation. Currently awaiting new wsdl with key worker details and summary needs to return a list

---
+++ ripple-stft-tie/src/main/java/org/rippleosi/patient/keyworker/search/HSCIEKeyWorkerSearch.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2015 Ripple OSI
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.rippleosi.patient.keyworker.search;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.xml.ws.soap.SOAPFaultException;
+
+import org.hscieripple.patient.keyworker.KWSummariesServiceSoap;
+import org.hscieripple.patient.keyworker.KWSummaryResponse;
+import org.rippleosi.common.service.AbstractHSCIEService;
+import org.rippleosi.patient.keyworkers.model.KeyWorkerSummary;
+import org.rippleosi.patient.keyworkers.search.KeyWorkerSearch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ */
+@Service
+public class HSCIEKeyWorkerSearch extends AbstractHSCIEService implements KeyWorkerSearch {
+
+    private static final Logger log = LoggerFactory.getLogger(HSCIEKeyWorkerSearch.class);
+
+    @Autowired
+    private KWSummariesServiceSoap kwSummariesService;
+
+    @Override
+    public List<KeyWorkerSummary> findAllKeyWorkers(String patientId) {
+        Long nhsNumber = patientId == null ? null : Long.valueOf(patientId);
+
+        try {
+            KWSummaryResponse kwSummaryResponse = kwSummariesService.findKWSummariesBO(nhsNumber, "test");
+            return Collections.singletonList(new KeyWorkerResponseToKeyWorkerSummaryTransformer().transform(kwSummaryResponse));
+        } catch (SOAPFaultException e) {
+            log.error(e.getMessage());
+
+            return new ArrayList<>();
+        }
+    }
+}
---
+++ ripple-stft-tie/src/main/java/org/rippleosi/patient/keyworker/search/KeyWorkerResponseToKeyWorkerSummaryTransformer.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2015 Ripple OSI
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.rippleosi.patient.keyworker.search;
+
+import org.apache.commons.collections4.Transformer;
+import org.hscieripple.patient.keyworker.KWSummaryResponse;
+import org.rippleosi.patient.keyworkers.model.KeyWorkerSummary;
+
+/**
+ */
+public class KeyWorkerResponseToKeyWorkerSummaryTransformer implements Transformer<KWSummaryResponse, KeyWorkerSummary> {
+
+    @Override
+    public KeyWorkerSummary transform(KWSummaryResponse response) {
+
+        KeyWorkerSummary summary = new KeyWorkerSummary();
+        summary.setContactNumber(response.getContactNumber());
+        summary.setName(response.getName());
+        summary.setRole(response.getRole());
+        summary.setSource(response.getDataSourceName());
+//        summary.setSourceId();
+
+        return summary;
+    }
+}
