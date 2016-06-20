902eb8bc24b8a8c9a1e60976b15632ddb98dad3e
Luke Garner <luke.garner@answerdigital.com>
2016-06-20 14:08:23 +0100

    STRD-54 Minor changes to add the license

--- hscie-ripple-core/src/main/java/org/hscieripple/audit/model/AuditDetails.java
+++ hscie-ripple-core/src/main/java/org/hscieripple/audit/model/AuditDetails.java
@@ -1,5 +1,17 @@
-/**
- * 
+/*
+ *   Copyright 2016 Ripple OSI
+ *
+ *      Licensed under the Apache License, Version 2.0 (the "License");
+ *      you may not use this file except in compliance with the License.
+ *      You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *      Unless required by applicable law or agreed to in writing, software
+ *      distributed under the License is distributed on an "AS IS" BASIS,
+ *      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *      See the License for the specific language governing permissions and
+ *      limitations under the License.
  */
 package org.hscieripple.audit.model;
 
@@ -9,8 +21,6 @@ import java.util.Date;
 /**
  * An audit record of access to patient data. Captures data regarding which user accessed which resource 
  * and at what time. In addition it captures the ID of the patient that the requested resource relates to.
- * 
- * @author WeatherillW
  *
  */
 public class AuditDetails implements Serializable {
